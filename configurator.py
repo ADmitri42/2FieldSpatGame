ce6050705119e4060e33880c7cdc5a1c49acdb07
Dmitriy Antonov <admitri42@gmail.com>
2021-11-09 16:30:54 +0300

    feat: Added script for evaluation

---
+++ configurator.py
@@ -0,0 +1,35 @@
+import os
+import json
+
+
+def configure_workflow(config_file: str, def_override: bool = False):
+    with open(config_file) as f:
+        config = json.load(f)
+
+    path_to_results = os.path.join(config["results"]["dir"], config["results"]["name"])
+
+    try:
+        os.makedirs(path_to_results)
+    except FileExistsError:
+        if not def_override:
+            ans = input("Are you sure you want to override existing results?(y/[n]) ").lower()
+            if len(ans) == 0 or ans == "n":
+                exit(0)
+            elif len(ans) > 1 or ans != "y":
+                print("Unknown answer")
+                exit(1)
+
+    if not os.path.exists(config["fields"]["dir"]):
+        raise FileNotFoundError("File with fields doesn't exists")
+
+    for i in range(config["fields"]["quantity"]):
+        if not os.path.exists(os.path.join(config["fields"]["dir"], f"field_{config['fields']['size']}_{i}.npy")):
+            raise FileNotFoundError("Not enough fields")
+
+    config["parameters"]["lam"] = config["parameters"].get("lam", 1)
+    config["parameters"]["mu"] = config["parameters"].get("mu", 0)
+
+    with open(os.path.join(path_to_results, "info.json"), "w") as f:
+        json.dump(config, f)
+
+    return config, path_to_results
---
+++ evaluate.py
@@ -0,0 +1,88 @@
+import argparse
+import os
+from collections import Counter
+from itertools import product
+
+import numpy as np
+from spatgames import TwoFieldSpatialGame as GamePy
+from tqdm import tqdm
+
+from configurator import configure_workflow
+
+# Configure workflow
+parser = argparse.ArgumentParser(
+    description="Collects data about persistence and saves it.")
+parser.add_argument("config",
+                    metavar="config",
+                    type=str,
+                    help="JSON file with configuration")
+
+parser.add_argument("-y",
+                    dest="overwrite",
+                    action="store_true",
+                    help="If needed, answer yes")
+parser.set_defaults(overwrite=False)
+
+args = parser.parse_args()
+config, path_to_results = configure_workflow(args.config, args.overwrite)
+
+
+bs = list(product(config["parameters"]["b1"], config["parameters"]["b1"]))
+persistence_shape = (len(bs), config["fields"]["quantity"], 2)
+density_shape = (
+    len(bs),
+    config["fields"]["quantity"],
+    2,
+    config["measures"]["tsteps"] + 1,
+)
+game = GamePy(
+    config["fields"]["size"],
+    1.3,
+    1.3,
+    config["parameters"]["lam"],
+    config["parameters"]["mu"],
+    config["measures"]["persistence"]["start"],
+    config["measures"]["persistence"]["end"],
+)
+
+persistence = np.zeros(persistence_shape)
+density = np.zeros(density_shape)
+
+field_temp = os.path.join(config["fields"]["dir"],
+                          f"field_{config['fields']['size']}" + "_{0}.npy")
+
+# Evolution
+for i, b in tqdm(zip(range(len(bs)), bs), total=len(bs)):
+    game.b = b
+    for j in range(config["fields"]["quantity"]):
+        game.field = np.load(field_temp.format(j))
+        game.evolve(config["measures"]["tsteps"])
+        persistence[i, j] = game.persistence
+        density[i, j] = game.densities
+
+new_density = np.zeros((
+    len(config["parameters"]["b1"]),
+    len(config["parameters"]["b2"]),
+    config["fields"]["quantity"],
+    2,
+    config["measures"]["tsteps"] + 1,
+))
+new_persistence = np.zeros((
+    len(config["parameters"]["b1"]),
+    len(config["parameters"]["b2"]),
+    config["fields"]["quantity"],
+    2,
+))
+indeces = product(
+    range(len(config["parameters"]["b1"])),
+    range(len(config["parameters"]["b2"])),
+)
+
+for k, (i, j) in enumerate(indeces):
+    new_density[i, j] = density[k]
+    new_persistence[i, j] = persistence[k]
+density = new_density
+persistence = new_persistence
+
+np.save(os.path.join(path_to_results, "persistence.npy"), persistence)
+np.save(os.path.join(path_to_results, "density.npy"), density)
